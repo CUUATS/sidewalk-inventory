370e94c108bfb0d593ec22cee703828c59ce7fd3
Matt Yoder <myoder@ccrpc.org>
2015-07-29 15:37:04 -0500

    Added configuration file.

---
+++ production.py
@@ -0,0 +1,17 @@
+"""
+Production database configuration.
+"""
+
+# Path to the SDE connection file.
+DATA_PATH = r''
+
+# Names of the feature classes.
+SW_NAME = ''  # Sidewalks
+CR_NAME = ''  # Curb ramps
+CW_NAME = ''  # Crosswalks
+PS_NAME = ''  # Pedestrian signals
+SS_NAME = ''  # Sidewalk segments
+
+# Paths to CSV files for tracking progress.
+SEGMENT_CSV = r''
+QASTATUS_CSV = r''
