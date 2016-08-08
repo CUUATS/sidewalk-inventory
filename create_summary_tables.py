668df28852af10a6ef52ee69bec39a35359c0e8c
Matt Yoder <myoder@ccrpc.org>
2016-08-08 10:55:51 -0500

    Added output formats.

--- create_summary_tables.py
+++ create_summary_tables.py
@@ -1,3 +1,7 @@
+import argparse
+import csv
+import json
+import os
 from cuuats.datamodel import D
 from datamodel import CurbRamp, Crosswalk, PedestrianSignal, SidewalkSegment
 from debug import CR_PATH, CW_PATH, PS_PATH, SS_PATH
@@ -33,6 +37,23 @@ def sidewalk_table(query_set, summary_field, column_label):
 
     return results
 
+# Parse command line arguments.
+parser = argparse.ArgumentParser(
+    'Create summary tables for sidewalk network features.')
+parser.add_argument('-f', '--format', dest='format', default='json',
+                    choices=['csv', 'json'], help='format of outpot')
+parser.add_argument('output', help='output file or location')
+args = parser.parse_args()
+
+# Verify the output location.
+if args.format == 'csv':
+    assert os.path.isdir(args.output), \
+        'Output location must be a directory for CSV format'
+else:
+    assert os.path.isdir(os.path.dirname(args.output)), \
+        'Invalid output location'
+
+# Prepare the results dictionary.
 results = {
     'Sidewalk': {},
     'Crosswalk': {},
@@ -40,6 +61,20 @@ results = {
     'PedestrianSignal': {}
 }
 
+# Create sidewalk tables.
 ss = SidewalkSegment.objects.filter(SummaryCount=1)
 for (field, label) in SIDEWALK_SEGMENT_FIELDS:
     results['Sidewalk'][field] = sidewalk_table(ss, field, label)
+
+# Create the output file or files.
+if args.format == 'json':
+    with open(args.output, 'wb') as output_file:
+        json.dump(results, output_file)
+else:
+    for (feature_type, tables) in results.items():
+        for (var_name, table) in tables.items():
+            output_path = os.path.join(
+                args.output, feature_type + var_name + '.csv')
+            with open(output_path, 'wb') as output_file:
+                writer = csv.writer(output_file)
+                writer.writerows(table)
